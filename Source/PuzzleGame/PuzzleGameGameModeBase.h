dab781ee0159c38cd2a3a19bd5ae2a80e8df47ff
Tage Veflingstad <Tage.veflingstad@gmail.com>
2017-02-15 14:10:37 +0100

    Initial Commit

---
+++ Source/PuzzleGame/PuzzleGameGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PuzzleGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEGAME_API APuzzleGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
