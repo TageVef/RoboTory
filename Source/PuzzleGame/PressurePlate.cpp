0fafb1e7d23d94c43e776a2631935ee901bc8908
Thernol <Tage.veflingstad@gmail.com>
2017-02-17 15:55:32 +0100

    Made the PressurePlate class and making the PressurePlate check if it is being overlaped

---
+++ Source/PuzzleGame/PressurePlate.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PuzzleGame.h"
+#include "PressurePlate.h"
+
+
+// Sets default values
+APressurePlate::APressurePlate()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create Root Component
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+
+	// Create Mesh Component
+	PressurePlateMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pressure Plate Mesh"));
+
+	// Create TriggerVolume
+	CollisionBox = CreateDefaultSubobject<ATriggerVolume>(TEXT("Collision Box"));
+
+}
+
+// Called when the game starts or when spawned
+void APressurePlate::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APressurePlate::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (CollisionBox->IsOverlappingActor(ObjectThatActivates))
+	{
+		
+	}
+
+}
+
---
+++ Source/PuzzleGame/PressurePlate.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "PressurePlate.generated.h"
+
+UCLASS()
+class PUZZLEGAME_API APressurePlate : public AActor
+{
+	GENERATED_BODY()
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* PressurePlateMesh;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* CollisionBox;
+
+	UPROPERTY(EditAnywhere)
+	AActor* DoorThatOpens;
+
+	UPROPERTY(EditAnywhere)
+	AActor* ObjectThatActivates;
+
+	
+public:	
+	// Sets default values for this actor's properties
+	APressurePlate();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+};
